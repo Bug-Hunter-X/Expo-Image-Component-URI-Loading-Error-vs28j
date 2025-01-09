06b48cb2b80dc792016d38bf01f5109e9ba744eb
BugHunter-X <askzenai@gmail.com>
2025-01-09 09:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error typically occurs when using Expo's `Image` component with a URI that is either invalid or inaccessible.  The URI might point to a non-existent image, have incorrect permissions, or be malformed.  Another possibility is that the network request to fetch the image times out or fails due to network connectivity issues.
